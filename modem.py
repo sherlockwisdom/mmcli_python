ed9c8db3d639f17d682d9758bd93379822a218c7
Sherlock Wisdom <wisdomnji@gmail.com>
2021-06-15 16:32:16 +0100

    updated: modem information extraction successful

---
+++ modem.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+
+# modem has
+# - SMS
+# - USSD
+
+import subprocess
+from subprocess import Popen, PIPE
+
+class Modem():
+    imei=None
+    model=None
+    index=None
+    state=None
+    dbus_path=None
+    power_state=None
+    operator_code=None
+    operator_name=None
+
+    # private methods
+    def __f_layer_parse(self, data):
+        data = data.split('\n')
+        details = {}
+        m_detail=None
+        for output in data:
+            m_detail = output.split(': ')
+            if len(m_detail) < 2:
+                continue
+            key = m_detail[0].replace(' ', '')
+            details[key] = m_detail[1]
+
+        return details
+
+    def __build_attributes(self, data):
+        self.imei = data["modem.3gpp.imei"]
+        self.state = data["modem.generic.state"]
+        self.model = data["modem.generic.model"]
+        self.dbus_path = data["modem.dbus-path"]
+        self.power_state = data["modem.generic.power-state"]
+        self.operator_code = data["modem.3gpp.operator-code"]
+        self.operator_name = data["modem.3gpp.operator-name"]
+
+    # public methods
+    def __init__(self, index):
+        self.query_command = ["mmcli", f"-Km", index]
+        self.index = index
+        self.refresh()
+
+    def refresh(self):
+        data = self.__f_layer_parse(subprocess.check_output(self.query_command, stderr=subprocess.STDOUT).decode('utf-8'))
+        self.__build_attributes(data)
+
+if __name__ == "__main__":
+    import sys
+    modem = Modem(sys.argv[1])
+    print(f"- imei: {modem.imei}")
+    print(f"- model: {modem.model}")
+    print(f"- index: {modem.index}")
+    print(f"- state: {modem.state}")
+    print(f"- dbus path: {modem.dbus_path}")
+    print(f"- power state: {modem.power_state}")
+    print(f"- operator code: {modem.operator_code}")
+    print(f"- operator name: {modem.operator_name}")
