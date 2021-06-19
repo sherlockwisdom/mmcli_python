713168bbf51e899b27479f8023d3bf6971410f1b
sh3rlock <wisdomnji@gmail.com>
2021-06-19 13:13:53 +0000

    update: ussd commands get processed

--- modem.py
+++ modem.py
@@ -88,17 +88,53 @@ class USSD():
 
     def initiate(self, command):
         ussd_command = self.modem.query_command + [f"--3gpp-ussd-initiate={command}"]
-        # print( ussd_command )
         try: 
             mmcli_output = subprocess.check_output(ussd_command, stderr=subprocess.STDOUT).decode('utf-8')
         except subprocess.CalledProcessError as error:
-            # print(f"[stderr]>> return code[{error.returncode}], output[{error.output.decode('utf-8')}")
-            raise Exception(f"[stderr]>> return code[{error.returncode}], output[{error.output.decode('utf-8')}")
+            print(traceback.format_exc())
         else:
-            # remote 'new reply from network:'
             mmcli_output = mmcli_output.split(": ", 1)[1].split("'")[1]
-            # print( mmcli_output )
             return mmcli_output
+
+    def respond(self, command):
+        ussd_command = self.modem.query_command + [f"--3gpp-ussd-respond={command}"]
+        try: 
+            mmcli_output = subprocess.check_output(ussd_command, stderr=subprocess.STDOUT).decode('utf-8')
+        except subprocess.CalledProcessError as error:
+            print(traceback.format_exc())
+        else:
+            mmcli_output = mmcli_output.split(": '", 1)[1][:-1]
+            return mmcli_output
+
+    def cancel(self):
+        ussd_command = self.modem.query_command + [f"--3gpp-ussd-cancel"]
+        try: 
+            mmcli_output = subprocess.check_output(ussd_command, stderr=subprocess.STDOUT).decode('utf-8')
+        except subprocess.CalledProcessError as error:
+            print(traceback.format_exc())
+        else:
+            return True
+        
+        return False
+
+    def status(self):
+        ussd_command = self.modem.query_command + [f"--3gpp-ussd-status"]
+
+        try: 
+            mmcli_output = subprocess.check_output(ussd_command, stderr=subprocess.STDOUT).decode('utf-8')
+        except subprocess.CalledProcessError as error:
+            print(traceback.format_exc())
+        else:
+            mmcli_output = mmcli_output.split('\n')
+            s_details = {}
+            for output in mmcli_output:
+                s_detail = output.split(': ')
+                if len(s_detail) < 2:
+                    continue
+                key = s_detail[0].replace(' ', '')
+                s_details[key] = s_detail[1]
+
+            return s_details
 
 class Modem():
     imei=None
@@ -174,3 +210,7 @@ if __name__ == "__main__":
         print(f"- pdu-type: {sms.pdu_type}")
         print(f"- state: {sms.state}")
         print(f"- timestamp: {sms.timestamp}")
+
+    print('ussd initiate ', modem.ussd.initiate("*123#"))
+    print('ussd respond ', modem.ussd.respond("6"))
+    print('ussd respond ', modem.ussd.respond("4"))
